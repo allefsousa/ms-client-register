0539d1a0d3e016eef10fe0b35fadc3fd5e33c989
Allef Sousa <allefsousa_1@hotmail.com>
2023-06-11 01:09:26 -0300

    init

---
+++ src/main/kotlin/com/allef/register/msclientregister/MsClientRegisterApplication.kt
@@ -0,0 +1,11 @@
+package com.allef.register.msclientregister
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsClientRegisterApplication
+
+fun main(args: Array<String>) {
+    runApplication<MsClientRegisterApplication>(*args)
+}
---
+++ src/test/kotlin/com/allef/register/msclientregister/MsClientRegisterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.allef.register.msclientregister
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsClientRegisterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
